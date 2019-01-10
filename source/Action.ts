84f0c4a063631ccbce16d8e279b56d57900c0111
Ralph Wiedemeier <ralph@framefactory.ch>
2019-01-10 19:19:36 +0100

    state machine, work in progress

---
+++ source/Action.ts
@@ -0,0 +1,13 @@
+/**
+ * FF Typescript Foundation Library
+ * Copyright 2018 Ralph Wiedemeier, Frame Factory GmbH
+ *
+ * License: MIT
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+
+export default class Action
+{
+    name: string;
+}
