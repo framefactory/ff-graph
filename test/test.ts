42470401bf513a270738c18f204341f0bfcb8526
Ralph Wiedemeier <ralph@framefactory.ch>
2020-05-01 16:44:06 +0200

    updated configuration

--- test/test.ts
+++ test/test.ts
@@ -5,16 +5,23 @@
  * License: MIT
  */
 
-import NodeComponent_test from "./NodeComponent.test";
-import Property_test from "./Property.test";
+import * as path from "path";
+import * as moduleAlias from "module-alias";
 
-////////////////////////////////////////////////////////////////////////////////
+moduleAlias.addAliases({
+    "@ff/graph": path.resolve(__dirname, "../export"),
+    "@ff/core": path.resolve(__dirname, "../../../ff-core/built/export"),
+});
 
-// define global vars on node global object
+// define vars on node global object (usually done by Webpack)
 global["ENV_DEVELOPMENT"] = false;
 global["ENV_PRODUCTION"] = true;
 global["ENV_VERSION"] = "Test";
 
+import NodeComponent_test from "./NodeComponent.test";
+import Property_test from "./Property.test";
+
+////////////////////////////////////////////////////////////////////////////////
 
 suite("FF Graph", function() {
     NodeComponent_test();
